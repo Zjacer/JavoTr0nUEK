67576419d9b34a8a41313428ba48e8a1e66016ab
Mateusz Zajac <zjacer@gmail.com>
2017-06-10 09:33:48 +0200

    Some GUI and logic work done in game client

--- src/com/zjacer/javotr0n/client/ClientHandler.java
+++ src/com/zjacer/javotr0n/client/ClientHandler.java
@@ -8,17 +8,18 @@ import com.zjacer.javotr0n.SomeRequest;
 import com.zjacer.javotr0n.SomeResponse;
 import java.io.IOException;
 import java.net.InetAddress;
+import javax.swing.JOptionPane;
 
 /**
  * @author Mateusz Zając @ zjacer@gmail.com
  */
 public class ClientHandler {
 
-    private GameClient gameServer;
+    private GameClient gameClient;
     Client client = new Client(54555, 54777);
 
-    public ClientHandler(GameClient gameServer) {
-        this.gameServer = gameServer;
+    public ClientHandler(GameClient gameClient) {
+        this.gameClient = gameClient;
     }
 
     public void start() {
@@ -30,14 +31,22 @@ public class ClientHandler {
         
         // LAN server discovery
         InetAddress address = client.discoverHost(54777, 5000);
-        System.out.println(address);
-    
+        
+        if(address == null) {
+            JOptionPane.showMessageDialog(gameClient,
+                "Server not found! You can try to type server's IP address manually.",
+                "Inane error",
+                JOptionPane.ERROR_MESSAGE);
+            stop();
+        }
+     
         try {
+            gameClient.getIpTextArea().setText("Server IP address: " + address.toString());
             client.connect(5000, address, 54555, 54777);
         } catch (IOException ex) {
             System.out.println("No server!");
         }
-        
+                  
         client.addListener(new Listener() {
             @Override
             public void received (Connection connection, Object object) {
--- src/com/zjacer/javotr0n/client/GameClient.java
+++ src/com/zjacer/javotr0n/client/GameClient.java
@@ -2,8 +2,13 @@ package com.zjacer.javotr0n.client;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * @author Mateusz Zając @ zjacer@gmail.com
@@ -12,7 +17,10 @@ public class GameClient extends JFrame {
 
     private JPanel contentPane;
     public ClientHandler handler;
-
+    private JButton readyButton;
+    private JButton notReadyButton;
+    private JLabel ipTextArea;
+    
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             @Override
@@ -32,12 +40,47 @@ public class GameClient extends JFrame {
         setTitle("JavoTr0n Client");
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 400, 400);
+        setBounds(100, 100, 600, 600);
+        readyButton = new JButton("READY");
+        notReadyButton = new JButton("NOT READY");
+        ipTextArea = new JLabel("Server not found.");
         contentPane = new JPanel();
         contentPane.setLayout(new BorderLayout());
         setContentPane(contentPane);
         
+        JPanel panel = new JPanel();
+        contentPane.add(panel, BorderLayout.NORTH);
+        
+        readyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchButtonsState();
+            }
+        });
+  
+        notReadyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchButtonsState();
+            }
+        });
+        
+        notReadyButton.setEnabled(false);
+        
+        panel.add(readyButton);
+        panel.add(notReadyButton);
+        panel.add(ipTextArea);
+        
         handler = new ClientHandler(this);
         handler.start();
+    }
+    
+    public void switchButtonsState() {
+        readyButton.setEnabled(!readyButton.isEnabled());
+        notReadyButton.setEnabled(!notReadyButton.isEnabled());
+    }
+    
+    public JLabel getIpTextArea() {
+        return ipTextArea;
     }
 }
