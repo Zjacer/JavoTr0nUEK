6ec0c3a3134e4a77feb06634321fb52fce1d71dc
Mateusz Zajac <zjacer@gmail.com>
2017-06-17 19:26:42 +0200

    KeyListener implemented for ClientHandler

--- src/com/zjacer/javotr0n/client/ClientHandler.java
+++ src/com/zjacer/javotr0n/client/ClientHandler.java
@@ -6,6 +6,8 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.zjacer.javotr0n.SomeRequest;
 import com.zjacer.javotr0n.SomeResponse;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.net.InetAddress;
 import javax.swing.JOptionPane;
@@ -13,11 +15,12 @@ import javax.swing.JOptionPane;
 /**
  * @author Mateusz Zając @ zjacer@gmail.com
  */
-public class ClientHandler {
+public class ClientHandler implements KeyListener {
 
     private GameClient gameClient;
     private Client client;
     private int portTCP, portUDP;
+    private boolean isPlaying = false;
     
     public ClientHandler(GameClient gameClient, int portTCP, int portUDP) {
         this.client = new Client(portTCP, portUDP);
@@ -77,5 +80,43 @@ public class ClientHandler {
     public void stop() {
         client.close();
         client.stop();
-    }                    
+    }
+ 
+    @Override
+    public void keyPressed(KeyEvent e) {
+ 
+        int key = e.getKeyCode();
+        String direction = "";
+ 
+        switch (key) {
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
+                direction = "RIGHT";
+                break;
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
+                direction = "UP";
+                break;
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
+                direction = "LEFT";
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
+                direction = "DOWN";
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
